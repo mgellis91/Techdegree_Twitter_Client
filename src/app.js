22e6baf950e6b6ec73f748c07761de8a6c028e21
Matt Ellis <mgellis91@gmail.com>
2017-02-01 15:29:58 +1100

    techdegree twitter client

---
+++ src/app.js
@@ -0,0 +1,74 @@
+
+var express = require("express");
+var EventEmitter  = require("events");
+var config = require("./config.js").twitterConfig;
+
+var app = express();
+var eventEmitter = new EventEmitter();
+var Twitter = require('twitter-node-client').Twitter;
+
+var user = null;
+var userId = null;
+var timeline = null;
+var friends = null;
+var messages = null;
+
+var twitter = new Twitter(config);
+
+var error = function (err, response, body) {
+    console.dir(err);
+};
+
+var twitterTimelineSuccess = function (data) {
+  var JSONdata = JSON.parse(data);
+  timeline = JSONdata;
+  twitter.getCustomApiCall('/friends/list.json',{ id: config.twitterId, count: config.friendsCount}, error, twitterFriendSuccess);
+}
+
+var twitterFriendSuccess = function (data) {
+  var JSONdata = JSON.parse(data);
+  friends = JSONdata;
+  twitter.getCustomApiCall('/direct_messages.json',{count: config.messageCount},error,twitterMessagesSuccess);
+}
+
+var twitterMessagesSuccess = function (data) {
+  var JSONdata = JSON.parse(data);
+  messages = JSONdata.reverse();
+  eventEmitter.emit("twitterData");
+}
+
+function getTwitterData () {
+  twitter.getUserTimeline({id: config.twitterId, count: twitter.timelineCount}, error, twitterTimelineSuccess);
+}
+
+function formatDateAndTime (timestamp) {
+
+}
+
+eventEmitter.on("twitterData",function () {
+  app.get("/", function(req,res){
+    res.locals.timeline = timeline;
+    res.locals.friends = friends.users;
+    res.locals.messages = messages;
+    res.locals.profile_image_url = timeline[0].user.profile_image_url;
+    res.locals.screen_name = timeline[0].user.screen_name;
+    res.render("index");
+  });
+});
+
+getTwitterData();
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.set("view engine", "jade");
+app.set("views", __dirname + "/views");
+
+
+app.post("/form",function (req,res) {
+  console.log("form submission");
+})
+
+
+app.listen(3000,function(){
+  console.log("running app at localhost:3000");
+});
---
+++ src/configTemplate.js
@@ -0,0 +1,14 @@
+
+var twitterConfig = {
+    "consumerKey": "your own consumer key",
+    "consumerSecret": "your own consumer secret",
+    "accessToken": "your own access token",
+    "accessTokenSecret": "your own access Token Secret",
+    "twitterId" : "your own twitter handle", //eg @mgellis91
+    "timelineCount" : 5, //default number of time line posts to dsiplay
+    "friendsCount" : 5, //default number of friends to display
+    "messagesCount" : 5 //default number of recent messages to display
+};
+
+
+module.exports.twitterConfig = twitterConfig;
